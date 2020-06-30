bd154b53388d20d2006d29550f15549d29b1dec5
Bryan Musungu <bryanmusungu@Bryans-MBP.hsd1.dc.comcast.net>
2020-06-30 09:23:15 -0400

    Fixed the delete

--- DogAppFrontEnd/src/app/doglist/doglist.component.ts
+++ DogAppFrontEnd/src/app/doglist/doglist.component.ts
@@ -1,6 +1,7 @@
 import { Dogs } from './../models/Dogs';
 import { Component, OnInit } from '@angular/core';
 import { DogsService } from '../services/dogs.service';
+import { error } from 'protractor';
 
 
 @Component({
@@ -20,10 +21,24 @@ export class DoglistComponent implements OnInit {
           console.log(data);
           this.listofDogs = data;
         },
-        (error: any)=>{
+        (error: Error)=>{
           console.log("An error occurred");
+          console.log(error.message);
         }
       )
+
+  }
+
+
+  deleteDog(id: number): void{
+    console.log("This current id: " + id);
+    this.dogServ.deleteDog(id).subscribe(
+      (response: Response)=>{
+      console.log(response.status);
+     },(error: any)=>{
+       console.log("Error during deletion: " + error);
+     }
+    )
 
   }
 
