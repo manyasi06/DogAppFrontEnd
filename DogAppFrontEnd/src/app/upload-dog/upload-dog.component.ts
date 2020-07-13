36660960d013f86e0fef373a70283c9d11ec201a
manyasi06 <bryan.musungu@gmail.com>
2020-07-13 15:15:06 -0700

    working on uploading

---
+++ DogAppFrontEnd/src/app/upload-dog/upload-dog.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-dog',
+  templateUrl: './upload-dog.component.html',
+  styleUrls: ['./upload-dog.component.css']
+})
+export class UploadDogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  onSubmit(){
+
+  }
+
+}
