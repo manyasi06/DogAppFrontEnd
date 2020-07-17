56efb3e888031cfbc1877511cce66bc8b1a8dbda
manyasi06 <bryan.musungu@gmail.com>
2020-07-17 08:26:46 -0700

    fix the issues with unit testing still needed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DogAppFrontEnd!');
+    expect(compiled.querySelector('h1').textContent).toContain('Dog Friend App');
   });
 });
