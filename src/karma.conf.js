7c16429442db42effee288c90add54013ee2a2e3
manyasi06 <bryan.musungu@gmail.com>
2020-07-20 07:02:00 -0700

    fix the issue regarding travis ci

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,16 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
+    customeLaunchers: {
+      HeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--proxy-auto-detect'
+        ]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
